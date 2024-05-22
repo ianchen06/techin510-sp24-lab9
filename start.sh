e14af9b54c814c5246c62f220da9e1ba840322ae
Ian Chen <ianchen06@gmail.com>
2024-05-22 07:06:16 -0700

    update start.sh script

--- start.sh
+++ start.sh
@@ -1,1 +1,9 @@
-FLASK_DEBUG=1 flask --app app run -p 5001
+#!/bin/bash
+if [ "$1" == "dev" ]; then
+    FLASK_DEBUG=1 flask --app app run -p 5001
+elif [ "$1" == "prod" ]; then
+    gunicorn -b :5001 --access-logfile - --error-logfile - app:app
+else
+    echo "Invalid argument. Please use 'dev' or 'prod'."
+    exit 1
+fi
